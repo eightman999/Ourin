815e7905af40554eb01ef9f6bd98c109388be6fd
eightman <eightman124@gmail.com>
2025-07-26 23:11:42 +0900

    Initial Commit

---
+++ MacUkagaka/MacUkagakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacUkagakaApp.swift
+//  MacUkagaka
+//
+//  Created by eightman on 2025/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MacUkagakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacUkagakaTests/MacUkagakaTests.swift
@@ -0,0 +1,16 @@
+//
+//  MacUkagakaTests.swift
+//  MacUkagakaTests
+//
+//  Created by eightman on 2025/07/26.
+//
+
+import Testing
+
+struct MacUkagakaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
